c561097925c0074e6720465285875928dabc7f7a
pedrohzf <pedrohzf@hotmail.com>
2020-04-03 00:07:32 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,21 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var souFoda = ['Pedro Henrique', 'Zomer', 31, 191, true]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function souMaisFoda(arrayzera) {
+	return arrayzera
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+function souMaisFoda2(slot2){
+	return slot2[1]
+}
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +24,20 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
-
+function souMaisFodaAinda(arrayzera, x){
+	return arrayzera[x]
+}
+	
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var souFoda = ['Pedro Henrique', undefined, null, 191, true]
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+souMaisFoda(souFoda)
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,7 +53,18 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(sejaFoda){
+		var objetoFoda = {
+		'carandiru': {quantidadePaginas: 250, autor: 'Drauzio Varela', editora: 'Ática'},
+		'tresCeus': {quantidadePaginas: 200, autor: 'Zezinho', editora: 'Abril'},
+		'aZonadaMorte': {quantidadePaginas: 350, autor: 'Paul Craig', editora: 'USA'}
+	}
+	if (sejaFoda != undefined) {
+		return objetoFoda[sejaFoda]
+	} else {
+		return objetoFoda
+	}
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
